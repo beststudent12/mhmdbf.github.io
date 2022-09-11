6f058eeca370a547ca8cc3b1533bb294f9e4ac5c
beststudent12 <103903678+beststudent12@users.noreply.github.com>
2022-09-11 20:50:11 +0200

    mhmd-intial version

---
+++ spiderman/f2.js
@@ -0,0 +1,7 @@
+function togglevideo(){
+    const trailer = document.querySelector('.trailer');
+    trailer.classList.toggle('active')
+    const video =document.querySelector('video');
+    video.currentTime=0;
+    video.pause();
+}
